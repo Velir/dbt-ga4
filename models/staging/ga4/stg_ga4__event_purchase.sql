44adc493ec66278177524a42c7630ecb69042a16
Adam Ribaudo <adam.ribaudo@velir.com>
2022-04-10 11:54:59 -0400

    further event cleanup

--- models/staging/ga4/stg_ga4__event_purchase.sql
+++ models/staging/ga4/stg_ga4__event_purchase.sql
@@ -1,52 +1,12 @@
-with purchase_with_params as (
-    select * 
-    FROM {{ref('stg_ga4__events')}},
-        UNNEST(event_params) as params
-    where event_name = 'purchase' -- Pull only 'purchase' events 
-        and 
-        (
-            params.key = 'page_location' or 
-            params.key = 'ga_session_number' or
-            params.key = 'page_referrer' or
-            params.key = 'currency' or
-            params.key = 'value' or 
-            params.key = 'payment_type' or 
-            params.key = 'coupon' or 
-            params.key = 'transaction_id'
-        )
-),
-pivoted as (
-select 
-    event_date_dt, 
-    event_key,
-    client_id, 
-    user_id,
-    event_timestamp, 
-    event_name, 
-    traffic_source_campaign_name,
-    traffic_source_source,
-    traffic_source_medium,
-    session_key,
-    MAX(if(key = "page_location", value.string_value, NULL)) as page_location,
-    MAX(if(key = "ga_session_id", value.int_value, NULL)) as ga_session_id,
-    MAX(if(key = "ga_session_number", value.int_value, NULL)) as ga_session_number,
-    MAX(if(key = "page_referrer", value.string_value, NULL)) as page_referrer,
-    MAX(if(key = "coupon", value.string_value, NULL)) as coupon,
-    MAX(if(key = "transaction_id", value.string_value, NULL)) as transaction_id,
-    MAX(if(key = "currency", value.string_value, NULL)) as currency,
-    MAX(if(key = "payment_type", value.string_value, NULL)) as payment_type,
-    MAX(if(key = "value", value.float_value, NULL)) as value
-    -- TODO how to handle items array?
-    
-from purchase_with_params
-group by 1,2,3,4,5,6,7,8,9,10
+ with purchase_with_params as (
+   select *,
+      {{ unnest_key('event_params', 'coupon') }},
+      {{ unnest_key('event_params', 'transaction_id') }},
+      {{ unnest_key('event_params', 'currency') }},
+      {{ unnest_key('event_params', 'payment_type') }},
+      {{ unnest_key('event_params', 'value', 'float_value') }}
+ from {{ref('stg_ga4__events')}}    
+ where event_name = 'purchase'
 )
 
-select 
-    *,
-    {{extract_hostname_from_url('page_location')}} as page_hostname,
-    case
-        when ga_session_number = 1 then TRUE
-        else FALSE
-    end as is_new_user
-from pivoted
+select * from purchase_with_params
--- models/staging/ga4/stg_ga4__event_session_start.sql
+++ models/staging/ga4/stg_ga4__event_session_start.sql
@@ -1,37 +1,11 @@
-with session_start_with_params as (
-    select *
-    FROM {{ref('stg_ga4__events')}} events,
-        UNNEST(event_params) as params
-    where event_name = 'session_start' 
-        and (
-            params.key = 'page_location' or 
-            params.key = 'ga_session_number'
-            )
-),
-pivoted as (
-select 
-    event_date_dt, 
-    event_key,
-    client_id, 
-    user_id,
-    event_timestamp, 
-    event_name, 
-    traffic_source_campaign_name,
-    traffic_source_source,
-    traffic_source_medium,
-    session_key,
-    MAX(if(key = "page_location", value.string_value, NULL)) as page_location,
-    MAX(if(key = "ga_session_id", value.int_value, NULL)) as ga_session_id,
-    MAX(if(key = "ga_session_number", value.int_value, NULL)) as ga_session_number,
-from session_start_with_params
-group by 1,2,3,4,5,6,7,8,9,10
+ with session_start_with_params as (
+   select *,
+      {{ unnest_key('event_params', 'entrances',  'int_value') }},
+      {{ unnest_key('event_params', 'page_title') }},
+      {{ unnest_key('event_params', 'page_referrer') }},
+      {{ unnest_key('event_params', 'value', 'float_value') }}
+ from {{ref('stg_ga4__events')}}    
+ where event_name = 'session_start'
 )
 
-select 
-    *,
-    {{extract_hostname_from_url('page_location')}} as page_hostname,
-    case
-        when ga_session_number = 1 then TRUE
-        else FALSE
-    end as is_new_user
-from pivoted
+select * from session_start_with_params
