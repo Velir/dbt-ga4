f7629cbc5ec6bd7a8bcf13d8216135ddd00259b6
Adam Ribaudo <adam.ribaudo@velir.com>
2022-04-10 13:45:45 -0400

    truly unique event keys now. unique test now passes

--- models/staging/ga4/stg_ga4__events.sql
+++ models/staging/ga4/stg_ga4__events.sql
@@ -43,11 +43,16 @@ include_session_key as (
         md5(CONCAT(stream_id, client_id, cast(ga_session_id as STRING))) as session_key -- Surrogate key to determine unique session across streams and users. Sessions do NOT reset after midnight in GA4
     from renamed
 ),
+include_event_number as (
+    select include_session_key.*,
+        row_number() over(partition by session_key) as session_event_number -- Number each event within a session to help generate a uniqu event key
+    from include_session_key
+),
 include_event_key as (
     select 
-        include_session_key.*,
-        md5(CONCAT(event_name, CAST(event_timestamp as STRING), CAST(TO_BASE64(session_key) as STRING))) as event_key -- Surrogate key for unique events
-    from include_session_key
+        include_event_number.*,
+        md5(CONCAT(CAST(TO_BASE64(session_key) as STRING), CAST(session_event_number as STRING))) as event_key -- Surrogate key for unique events
+    from include_event_number
 ),
 enrich_params as (
     select 
