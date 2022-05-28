bc598ec181041c791f14b7cbf42ba5dba9cc779f
Adam Ribaudo <adam.ribaudo@velir.com>
2022-05-28 10:24:54 -0400

    disable user properties model if no custom user properties exist

--- models/staging/ga4/stg_ga4__user_properties.sql
+++ models/staging/ga4/stg_ga4__user_properties.sql
@@ -1,5 +1,5 @@
 {{ config(
-  enabled = true if var('user_properties') else false 
+  enabled = true if var('user_properties', false) != false else false
 ) }}
 
 
@@ -8,7 +8,7 @@ with unnest_user_properties as
     select 
         client_id,
         event_timestamp
-        {% for up in var('user_properties') %}
+        {% for up in var('user_properties', []) %}
             ,{{ unnest_key('event_params',  up.event_parameter ,  up.value_type ) }}
         {% endfor %}
     from {{ref('stg_ga4__events')}}
@@ -17,7 +17,7 @@ find_last_value as
 (
     select
         client_id
-        {% for up in var('user_properties') %}
+        {% for up in var('user_properties', []) %}
             ,LAST_VALUE({{ up.event_parameter }}) OVER (PARTITION BY client_id ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS {{up.user_property_name}}
         {% endfor %}
     from unnest_user_properties
