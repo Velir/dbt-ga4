ea26e8334fed90bb08cc48ce1041ad6765704ee0
Adam Ribaudo <adam.ribaudo@velir.com>
2022-06-21 21:00:28 -0400

    load in fivetran utils and first attempt at union_data

---
+++ models/staging/ga4/base/base_multiple_exports.sql
@@ -0,0 +1,10 @@
+{{
+    fivetran_utils.union_data(
+        table_identifier='account', 
+        database_variable='xero_database', 
+        schema_variable='xero_schema', 
+        default_database=target.database,
+        default_schema='xero',
+        default_variable='account'
+    )
+}} 
