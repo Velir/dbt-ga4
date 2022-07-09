db166d0c2c26a2359ad0f9479487589f71b937ca
Adam Ribaudo <adam.ribaudo@velir.com>
2022-07-09 10:47:38 -0400

    added some helpful page-level dimensions
    from https://www.ga4bigquery.com/page-tracking-dimensions-metrics-ga4/

--- models/staging/ga4/events/stg_ga4__event_page_view.sql
+++ models/staging/ga4/events/stg_ga4__event_page_view.sql
@@ -1,7 +1,13 @@
  with page_view_with_params as (
    select *,
       {{ ga4.unnest_key('event_params', 'entrances',  'int_value') }},
-      {{ ga4.unnest_key('event_params', 'value', 'float_value') }}
+      {{ ga4.unnest_key('event_params', 'value', 'float_value') }},
+      lag(page_location, 1) over (partition by (session_key) order by event_timestamp asc) as previous_page_location_in_session,
+      case when split(split(page_location,'/')[safe_ordinal(4)],'?')[safe_ordinal(1)] = '' then null else concat('/',split(split(page_location,'/')[safe_ordinal(4)],'?')[safe_ordinal(1)]) end as pagepath_level_1,
+      case when split(split(page_location,'/')[safe_ordinal(5)],'?')[safe_ordinal(1)] = '' then null else concat('/',split(split(page_location,'/')[safe_ordinal(5)],'?')[safe_ordinal(1)]) end as pagepath_level_2,
+      case when split(split(page_location,'/')[safe_ordinal(6)],'?')[safe_ordinal(1)] = '' then null else concat('/',split(split(page_location,'/')[safe_ordinal(6)],'?')[safe_ordinal(1)]) end as pagepath_level_3,
+      case when split(split(page_location,'/')[safe_ordinal(7)],'?')[safe_ordinal(1)] = '' then null else concat('/',split(split(page_location,'/')[safe_ordinal(7)],'?')[safe_ordinal(1)]) end as pagepath_level_4,
+
       {% if var("page_view_custom_parameters", "none") != "none" %}
         {{ stage_custom_parameters( var("page_view_custom_parameters") )}}
       {% endif %}
@@ -11,7 +17,7 @@
 first_last_pageview_session as (
   select * from {{ref('stg_ga4__sessions_first_last_pageviews')}}
 ),
--- Determine the session's first pageview pageview based on event_timestamp. This is redundant with the 'entrances' int value, but calculated in the warehouse so a bit more transparent in how it operates. 
+-- Determine the session's first pageview based on event_timestamp. This is redundant with the 'entrances' int value, but calculated in the warehouse so a bit more transparent in how it operates. 
 first_pageview_joined as (
   select 
     page_view_with_params.*,
