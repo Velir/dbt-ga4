4a37e6c07f6084dae690d4cc950f7e5849659808
Damon Gudaitis <damon@caretjuice.com>
2022-08-02 13:50:41 -0700

    Added login, search, and sign_up recommended events

---
+++ models/staging/ga4/recommended_events/stg_ga4__event_login.sql
@@ -0,0 +1,16 @@
+{{
+  config(
+      enabled = false,
+  )
+}}
+
+ with login_with_params as (
+   select *
+      {% if var("login_custom_parameters", "none") != "none" %}
+        {{ ga4.stage_custom_parameters( var("login_custom_parameters") )}}
+      {% endif %}
+ from {{ref('stg_ga4__events')}}
+ where event_name = 'login'
+)
+
+select * from login_with_params
