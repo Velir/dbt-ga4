683348f196a7b96f3811ee05dee705c092bce571
Damon Gudaitis <damon@caretjuice.com>
2022-08-03 11:15:42 -0700

    incremental session tables

--- models/marts/core/fct_ga4__sessions.sql
+++ models/marts/core/fct_ga4__sessions.sql
@@ -1,4 +1,32 @@
 -- Fact table for sessions. Join on session_key
+{% if var('static_incremental_days', false ) %}
+    {% set partitions_to_replace = [] %}
+    {% for i in range(var('static_incremental_days')) %}
+        {% set partitions_to_replace = partitions_to_replace.append('date_sub(current_date, interval ' + (i+1)|string + ' day)') %}
+    {% endfor %}
+    {{
+        config(
+            materialized = 'incremental',
+            incremental_strategy = 'insert_overwrite',
+            partition_by={
+                "field": "session_start_date",
+                "data_type": "date",
+            },
+            partitions = partitions_to_replace,
+        )
+    }}
+{% else %}
+    {{
+        config(
+            materialized = 'incremental',
+            incremental_strategy = 'insert_overwrite',
+            partition_by={
+                "field": "session_start_date",
+                "data_type": "date",
+            },
+        )
+    }}
+{% endif %}
 
 with session_metrics as 
 (
@@ -12,6 +40,11 @@ with session_metrics as
         ifnull(max(session_engaged), 0) as session_engaged,
         sum(engagement_time_msec) as sum_engagement_time_msec
     from {{ref('stg_ga4__events')}}
+    {% if is_incremental() %}
+        {% if var('static_incremental_days', false ) %}
+            and session_start_date in ({{ partitions_to_replace | join(',') }})
+        {% endif %}
+    {% endif %}
     group by 1,2
 )
 {% if var('conversion_events',false) %}
