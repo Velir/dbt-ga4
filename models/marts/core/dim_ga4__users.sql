741ded737cbc9d5466bcfe2df794b6003c750385
Damon Gudaitis <damon@caretjuice.com>
2022-08-04 11:08:18 -0700

    add partition filter to stg_ga4__events

--- models/marts/core/dim_ga4__users.sql
+++ models/marts/core/dim_ga4__users.sql
@@ -1,62 +1,70 @@
 -- User dimensions: first geo, first device, last geo, last device, first seen, last seen
 {{ config(
-    materialized= 'table',
+    materialized= 'incremental',
+    unique_key='user_key'
 )
 }}
-with users as (
+
+with last_event_values as (  -- get the last session start event for each user
+    select
+        *
+    from {{ref('stg_ga4__users_last_events')}} 
+),
+last_pageview_values as (  -- get the last session start event for each user
+    select
+        *
+    from {{ref('stg_ga4__users_last_pageviews')}} 
+    left join last_event_values using (user_key)
+),
+first_session as (
+    select
+        *
+    from {{ref('stg_ga4__first_sessions')}}
+    right join last_pageview_values using (user_key) -- limit to users with sessions in the window of time that we are working on
+)
+{% if is_incremental() %} -- get current values and new values 
+,current_values as (
+    select
+        user_key
+        num_page_views,
+        num_purchases
+    from {{this}}
+    where user_key in users.user_key
+),
+last_modified as (  -- is there a better way to do this?
+    select
+        max(last_seen_timestamp) as last_modified
+    from {{this}}
+),
+
+new_values as (
     select 
         user_key,
-        min(event_timestamp) as first_seen_timestamp,
-        min(event_date_dt) as first_seen_dt,
-        max(event_timestamp) as last_seen_timestamp,
-        max(event_date_dt) as last_seen_dt,
-        count(distinct session_key) as num_sessions,
         sum(is_page_view) as num_page_views,
-        sum(is_purchase) as num_purchases
+        sum(is_purchase) as num_purchases,
     from {{ref('stg_ga4__events')}}
     where user_key is not null -- Remove users with privacy settings enabled
+    and event_timestamp > last_modified.last_modified
     group by 1
-
-),
-include_first_last_events as (
-    select 
-        users.*,
-        first_last_events.first_geo,
-        first_last_events.first_device,
-        first_last_events.first_traffic_source,
-        first_last_events.last_geo,
-        first_last_events.last_device,
-        first_last_events.last_traffic_source,
-    from users 
-    left join {{ref('stg_ga4__users_first_last_events')}} as first_last_events on
-        users.user_key = first_last_events.user_key
-),
-include_first_last_page_views as (
-    select 
-        include_first_last_events.*,
-        first_last_page_views.first_page_location,
-        first_last_page_views.first_page_hostname,
-        first_last_page_views.first_page_referrer,
-        first_last_page_views.last_page_location,
-        first_last_page_views.last_page_hostname,
-        first_last_page_views.last_page_referrer
-    from include_first_last_events 
-    left join {{ref('stg_ga4__users_first_last_pageviews')}} as first_last_page_views on
-        include_first_last_events.user_key = first_last_page_views.user_key
-),
-include_user_properties as (
-    
-
-select * from include_first_last_page_views
-{% if var('derived_user_properties', false) %}
--- If derived user properties have been assigned as variables, join them on the user_key
-left join {{ref('stg_ga4__derived_user_properties')}} using (user_key)
-{% endif %}
-{% if var('user_properties', false) %}
--- If user properties have been assigned as variables, join them on the user_key
-left join {{ref('stg_ga4__user_properties')}} using (user_key)
-{% endif %}
-
 )
+select
+    new_values.user_key,
+    sum(current_values.num_page_views + new_values.num_page_views) as num_page_views,
+    sum(current_values.num_page_purchases + new_values.num_purchases) as num_purchases
+from new_values
+left join current_values using (user_key)
+left join first_sessions using (user_key)
 
-select * from include_user_properties
+{% else %} -- build the table from scratch
+select 
+    user_key,
+    max(event_timestamp) as last_seen_timestamp,
+    max(event_date_dt) as last_seen_dt,
+    max(ga_session_number) as num_sessions,
+    sum(is_page_view) as num_page_views,
+    sum(is_purchase) as num_purchases
+from {{ref('stg_ga4__events')}}
+left join first_sessions using (user_key)
+where user_key is not null -- Remove users with privacy settings enabled
+group by 1
+{% endif %}
