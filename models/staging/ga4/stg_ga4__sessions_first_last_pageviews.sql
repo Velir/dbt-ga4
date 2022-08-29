f61b62382d58003129d004b44a759eceaffa74f2
Damon Gudaitis <damon@caretjuice.com>
2022-08-29 15:13:24 -0700

    use event_page_view instead of events table

--- models/staging/ga4/stg_ga4__sessions_first_last_pageviews.sql
+++ models/staging/ga4/stg_ga4__sessions_first_last_pageviews.sql
@@ -5,8 +5,7 @@ with page_views_first_last as (
         FIRST_VALUE(event_key) OVER (PARTITION BY session_key ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS first_page_view_event_key,
         LAST_VALUE(event_key) OVER (PARTITION BY session_key ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS last_page_view_event_key,
         ga_session_number
-    from {{ref('stg_ga4__events')}}
-    where event_name = 'page_view'
+    from {{ref('stg_ga4__event_page_view')}}
 ),
 page_views_by_session_key as (
     select distinct
