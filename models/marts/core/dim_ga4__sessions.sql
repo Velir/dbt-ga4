a551a39d90c1c0c64ba3d97df0d204e19748a3ab
Adam Ribaudo <adam.ribaudo@velir.com>
2022-09-04 16:10:50 -0400

    removing 2 fields in the marts affected by the optimization updates
    These mart models are going to change substantially, anyway.

--- models/marts/core/dim_ga4__sessions.sql
+++ models/marts/core/dim_ga4__sessions.sql
@@ -3,7 +3,6 @@
 with session_start_dims as (
     select 
         session_key,
-        ga_session_number,
         page_location as landing_page,
         page_hostname as landing_page_hostname,
         geo_continent,
@@ -31,23 +30,16 @@ with session_start_dims as (
         traffic_source_name,
         traffic_source_medium,
         traffic_source_source,
-        row_number() over (partition by session_key order by session_event_number asc) as row_num
     from {{ref('stg_ga4__event_session_start')}}
-),
--- Arbitrarily pull the first session_start event to remove duplicates
-remove_dupes as 
-(
-    select * from session_start_dims
-    where row_num = 1
 ),
 join_traffic_source as (
     select 
-        remove_dupes.*,
+        session_start_dims.*,
         session_source as source,
         session_medium as medium,
         session_campaign as campaign,
         session_default_channel_grouping as default_channel_grouping
-    from remove_dupes
+    from session_start_dims
     left join {{ref('stg_ga4__sessions_traffic_sources')}} using (session_key)
 )
 
