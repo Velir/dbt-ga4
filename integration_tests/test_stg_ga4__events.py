7d3910c3a6fe810092ada84451efad0be3f346bf
Adam Ribaudo <adam.ribaudo@velir.com>
2022-09-04 13:02:32 -0400

    fixing unit test

--- integration_tests/test_stg_ga4__events.py
+++ integration_tests/test_stg_ga4__events.py
@@ -2,20 +2,17 @@ import pytest
 from dbt.tests.util import read_file,check_relations_equal,run_dbt
 
 # Define mocks via CSV (seeds) or SQL (models)
-mock_base_ga4__events_csv = """user_id,user_pseudo_id,ga_session_id,stream_id,page_location,page_referrer
-user_id_1,user_pseudo_id_1,ga_session_id_1,stream_id_1,http://www.website.com/?foo=bar,http://www.cnn.com/
+mock_base_ga4__events_csv = """user_id,event_name,event_timestamp,user_pseudo_id,ga_session_id,stream_id,page_location,page_referrer
+user_id_1,pageview,12345,user_pseudo_id_1,ga_session_id_1,stream_id_1,http://www.website.com/?foo=bar,http://www.cnn.com/
 """.lstrip()
 
-#TODO
-#,user_pseudo_id_2,ga_session_id_1,stream_id_1,http://www.website.com
-
-expected_csv = """user_id,user_pseudo_id,ga_session_id,stream_id,user_key,session_key,session_event_number,event_key,original_page_location,original_page_referrer,page_location,page_referrer,page_hostname,page_query_string
-user_id_1,user_pseudo_id_1,ga_session_id_1,stream_id_1,c/nWU/GWhlWiLU0S6R/rwg==,9fDgaCrbd4ieAj1QpcWDjw==,1,FgBuqiZAGtlzSpZZgrY2VA==,http://www.website.com/?foo=bar,http://www.cnn.com/,http://www.website.com/?foo=bar,http://www.cnn.com/,website.com,foo=bar
+expected_csv = """user_id,event_name,event_timestamp,user_pseudo_id,ga_session_id,stream_id,user_key,session_key,event_key,original_page_location,original_page_referrer,page_location,page_referrer,page_hostname,page_query_string
+user_id_1,pageview,12345,user_pseudo_id_1,ga_session_id_1,stream_id_1,c/nWU/GWhlWiLU0S6R/rwg==,9fDgaCrbd4ieAj1QpcWDjw==,CuY6cWwqR4TDmeHhNHlnwQ==,http://www.website.com/?foo=bar,http://www.cnn.com/,http://www.website.com/?foo=bar,http://www.cnn.com/,website.com,foo=bar
 """
 
 actual = read_file('../models/staging/ga4/stg_ga4__events.sql')
 
-class TestUsersFirstLastEvents():
+class TestStgEvents():
     # everything that goes in the "seeds" directory (= CSV format)
     @pytest.fixture(scope="class")
     def seeds(self):
