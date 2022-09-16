02fcacdb4bd4833534ef57462e7a326e81389168
dgitis <damon@caretjuice.com>
2022-09-16 10:39:22 -0700

    add event date to sessions_first_last

--- models/staging/ga4/stg_ga4__sessions_first_last_events.sql
+++ models/staging/ga4/stg_ga4__sessions_first_last_events.sql
@@ -3,6 +3,7 @@ with events_first_last as (
         session_key,
         user_key,
         FIRST_VALUE(event_timestamp) OVER (PARTITION BY session_key ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS first_event_timestamp,
+        FIRST_VALUE(event_date_dt) OVER (PARTITION BY session_key ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS first_event_date_dt,
         FIRST_VALUE(event_key) OVER (PARTITION BY session_key ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS first_event_key,
         LAST_VALUE(event_key) OVER (PARTITION BY session_key ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS last_event_key,
     from {{ref('stg_ga4__events')}}
@@ -12,6 +13,7 @@ events_by_session_key as (
         session_key,
         user_key,
         first_event_timestamp,
+        first_event_date_dt,
         first_event_key,
         last_event_key
     from events_first_last
