499ece42ebf4fabd9568339126cde2d538510d27
dgitis <damon@caretjuice.com>
2022-09-16 10:45:08 -0700

    add page view date to sessions_sessions_first_last

--- models/staging/ga4/stg_ga4__sessions_first_last_pageviews.sql
+++ models/staging/ga4/stg_ga4__sessions_first_last_pageviews.sql
@@ -2,7 +2,8 @@ with page_views_first_last as (
     select
         session_key,
         user_key,
-        FIRST_VALUE(event_timestamp) OVER (PARTITION BY session_key ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS first_event_timestamp,
+        FIRST_VALUE(event_timestamp) OVER (PARTITION BY session_key ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS first_page_view_timestamp,
+        FIRST_VALUE(event_date_dt) OVER (PARTITION BY session_key ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS first_page_view_date_dt,
         FIRST_VALUE(event_key) OVER (PARTITION BY session_key ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS first_page_view_event_key,
         LAST_VALUE(event_key) OVER (PARTITION BY session_key ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS last_page_view_event_key,
     from {{ref('stg_ga4__events')}}
@@ -12,7 +13,8 @@ events_by_session_key as (
     select distinct
         session_key,
         user_key,
-        first_event_timestamp,
+        first_page_view_timestamp,
+        first_page_view_date,
         first_page_view_event_key,
         last_page_view_event_key
     from page_views_first_last
