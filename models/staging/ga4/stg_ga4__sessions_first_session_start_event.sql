4ab258d8fce126d71152dd77f2c9989a077e0e22
Adam Ribaudo <adam.ribaudo@velir.com>
2022-10-20 23:23:06 -0400

    slightly cleaner with qualify statement

--- models/staging/ga4/stg_ga4__sessions_first_session_start_event.sql
+++ models/staging/ga4/stg_ga4__sessions_first_session_start_event.sql
@@ -2,11 +2,12 @@ with session_start_events as
 (
     select * from {{ref('stg_ga4__event_session_start')}}
 ),
-session_start_row_number as 
+session_start_first_row_number as 
 (
-    select *,
-    row_number() over(partition by session_key order by event_timestamp) as session_event_number
+    select *
     from session_start_events
+    qualify row_number() over(partition by session_key order by event_timestamp) = 1
+    
 )
 
-select * from session_start_row_number where session_event_number = 1
+select * from session_start_first_row_number
