a7b7e4dcd35801f7356672bb6d39c12ba14342a9
Will Bryant <will.bryant@gmail.com>
2022-10-27 18:47:36 +1300

    refactor: Simplify derived user properties & session properties queries
    Use LAST_VALUE with IGNORE NULLS and DISTINCT instead of a set of extra CTEs and joins.
    
    To avoid 'Resources exceeded during query execution: Not enough resources for query planning - too many subqueries or query is too complex.' errors.

--- models/staging/ga4/stg_ga4__derived_user_properties.sql
+++ models/staging/ga4/stg_ga4__derived_user_properties.sql
@@ -18,54 +18,11 @@ unnest_user_properties as
         {% endfor %}
     from events_from_valid_users
 )
--- create 1 CTE per user property that pulls only events with non-null values for that event parameters. 
--- Find the most recent property for that user and join later
-{% for up in var('derived_user_properties', []) %}
-,non_null_{{up.event_parameter}} as
-(
-    select
-        user_key,
-        event_timestamp,
-        {{up.event_parameter}}
-    from unnest_user_properties
-    where
-        {{up.event_parameter}} is not null
-),
-last_value_{{up.event_parameter}} as 
-(
-    select
-        user_key,
-        LAST_VALUE({{ up.event_parameter }}) OVER (PARTITION BY user_key ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS {{up.user_property_name}}
-    from non_null_{{up.event_parameter}}
-),
-last_value_{{up.event_parameter}}_grouped as 
-(
-    select
-        user_key,
-        {{up.user_property_name}}
-    from last_value_{{up.event_parameter}}
-    group by user_key, {{up.user_property_name}}
-)
-{% endfor %}
-,
-user_keys as 
-(
-    select distinct
-        user_key
-    from events_from_valid_users
-),
-join_properties as 
-(
-    select
-        user_key
-        {% for up in var('derived_user_properties', []) %}
-        ,last_value_{{up.event_parameter}}_grouped.{{up.user_property_name}}
-        {% endfor %}
-    from user_keys
+
+SELECT DISTINCT
+    user_key
     {% for up in var('derived_user_properties', []) %}
-    left join last_value_{{up.event_parameter}}_grouped using (user_key)
+        , LAST_VALUE({{ up.event_parameter }} IGNORE NULLS) OVER (user_window) AS {{ up.user_property_name }}
     {% endfor %}
-)
-
-
-select distinct * from join_properties
+FROM unnest_user_properties
+WINDOW user_window AS (PARTITION BY user_key ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
