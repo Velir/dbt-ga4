8f20505f11c43c7743dd0525132d9f1a53ca3d5e
Adam Ribaudo <adam.ribaudo@velir.com>
2022-10-29 09:55:00 -0400

    replacing session facts table with incremental version

--- models/marts/core/fct_ga4__sessions.sql
+++ models/marts/core/fct_ga4__sessions.sql
@@ -1,38 +1,51 @@
--- Fact table for sessions. Join on session_key
+{{
+    config(
+        materialized = 'incremental',
+        incremental_strategy = 'insert_overwrite',
+        tags = ["incremental"],
+        partition_by={
+        "field": "session_start_date",
+        "data_type": "date",
+        "granularity": "day"
+        }
+    )
+}}
 
-with session_metrics as 
-(
-    select distinct
-        session_key,
-        last_value(user_key) over (session_window) as user_key,
-        first_value(event_date_dt) over (session_window) as session_start_date,
-        first_value(event_timestamp) over (session_window) as session_start_timestamp,
-        countif(event_name = 'page_view') over (session_window) as count_page_views,
-        sum(event_value_in_usd) over (session_window) as sum_event_value_in_usd,
-        ifnull(last_value(session_engaged) over (session_window), 0) as session_engaged,
-        sum(engagement_time_msec) over (session_window) as sum_engagement_time_msec
-    from {{ref('stg_ga4__events')}}
-    window session_window as (partition by session_key order by event_timestamp rows between unbounded preceding and unbounded following)
-),
+    with session_metrics as (
+        select 
+            session_key,
+            user_pseudo_id,
+            min(event_date_dt) as session_start_date,
+            min(event_timestamp) as session_start_timestamp,
+            countif(event_name = 'page_view') as count_page_views,
+            sum(event_value_in_usd) as sum_event_value_in_usd,
+            ifnull(max(session_engaged), 0) as session_engaged,
+            sum(engagement_time_msec) as sum_engagement_time_msec
+        from {{ref('stg_ga4__events')}}
+        -- Give 1 extra day to ensure we beging aggregation at the start of a session
+        where session_key is not null
+        {% if is_incremental() %}
+            and event_date_dt >= DATE_SUB(_dbt_max_partition, INTERVAL 1 DAY)
+        {% endif %}
+        group by 1,2
+    ),
+    session_conversions as (
+        select * from {{ref('stg_ga4__session_conversions')}}
+        {% if is_incremental() %}
+            where session_start_date >= DATE_SUB(_dbt_max_partition, INTERVAL 1 DAY)
+        {% endif %}
+    ),
+    join_metrics_and_conversions as (
+        select 
+            session_metrics.user_pseudo_id,
+            session_metrics.session_start_timestamp,
+            session_metrics.count_page_views,
+            session_metrics.sum_event_value_in_usd,
+            session_metrics.session_engaged,
+            session_metrics.sum_engagement_time_msec,
+            session_conversions.*
+        from session_metrics left join session_conversions using (session_key)
+    )
 
-include_session_properties as (
-    select * from session_metrics
-    {% if var('derived_session_properties', false) %}
-    -- If derived session properties have been assigned as variables, join them on the session_key
-    left join {{ref('stg_ga4__derived_session_properties')}} using (session_key)
-    {% endif %}
-)
-
-{% if var('conversion_events',false) %}
-,
-join_conversions as (
-    select 
-        *
-    from include_session_properties
-    left join {{ref('stg_ga4__session_conversions')}} using (session_key)
-)
-select * from join_conversions
-{% else %}
-select * from include_session_properties
-{% endif %}
+select * from join_metrics_and_conversions
 
