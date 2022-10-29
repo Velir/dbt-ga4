4135c318967fec67a9a721c9ea613b86d16b24f6
Adam Ribaudo <adam.ribaudo@velir.com>
2022-10-29 09:05:36 -0400

    changed to insert_overwrite. doesn't impact bytes billed positively

--- models/marts/core/core_ga4__sessions.sql
+++ models/marts/core/core_ga4__sessions.sql
@@ -1,8 +1,10 @@
+--FULL REFRESH: Bytes processed, 34.39 MB, Bytes billed 35 MB 
+--INCREMENTAL: Bytes processed 1.62 MB, Bytes billed  60 MB 
 {{
     config(
         materialized = 'incremental',
+        incremental_strategy = 'insert_overwrite',
         tags = ["incremental"],
-        unique_key='session_key',
         partition_by={
         "field": "session_start_date",
         "data_type": "date",
@@ -12,52 +14,37 @@
 }}
 
 {% if is_incremental() %}
-    -- Gather all sessions that started (as opposed to continued) in the date range we care about.
-    -- Give 1 extra day to account for sessions we have only partially prossed
-    with max_date_cte as (
-        select DATE_SUB(max(session_start_date), INTERVAL 1 DAY) as max_session_start_date_less_1 
-        from {{this}}
-    ), 
-    sessions_to_process as (
-        select 
-            session_key 
-        from {{ref('stg_ga4__events')}}
-        where event_date_dt >= 
-            (select max_session_start_date_less_1 from max_date_cte)
-        -- Use 'session_start' event to determine which new sessions to process
-        and event_name = 'session_start'    
-    ),
-    session_metrics as (
+    -- TODO: Give 1 extra day to account for sessions we have only partially prossed
+    with session_metrics as (
         select 
             session_key,
-            user_key,
             min(event_date_dt) as session_start_date,
-            min(event_timestamp) as session_start_timestamp,
-            countif(event_name = 'page_view') as count_page_views,
-            sum(event_value_in_usd) as sum_event_value_in_usd,
-            ifnull(max(session_engaged), 0) as session_engaged,
-            sum(engagement_time_msec) as sum_engagement_time_msec
+            --min(event_timestamp) as session_start_timestamp,
+            --countif(event_name = 'page_view') as count_page_views,
+            --sum(event_value_in_usd) as sum_event_value_in_usd,
+            --ifnull(max(session_engaged), 0) as session_engaged,
+            --sum(engagement_time_msec) as sum_engagement_time_msec
         from {{ref('stg_ga4__events')}}
         -- This documentation states that using a dynamic value to prune partitions will not work:
         -- https://cloud.google.com/bigquery/docs/querying-partitioned-tables#better_performance_with_pseudo-columns
-        where event_date_dt >= (select max_session_start_date_less_1 from max_date_cte)
-        and session_key in (select session_key from sessions_to_process)
-        group by 1,2
+        where event_date_dt >= _dbt_max_partition
+        and session_key is not null
+        group by 1
     )
 {% else %}
 
     with session_metrics as (
     select 
         session_key,
-        user_key,
         min(event_date_dt) as session_start_date,
-        min(event_timestamp) as session_start_timestamp,
-        countif(event_name = 'page_view') as count_page_views,
-        sum(event_value_in_usd) as sum_event_value_in_usd,
-        ifnull(max(session_engaged), 0) as session_engaged,
-        sum(engagement_time_msec) as sum_engagement_time_msec
+        --min(event_timestamp) as session_start_timestamp,
+        --countif(event_name = 'page_view') as count_page_views,
+        --sum(event_value_in_usd) as sum_event_value_in_usd,
+        --ifnull(max(session_engaged), 0) as session_engaged,
+        --sum(engagement_time_msec) as sum_engagement_time_msec
     from {{ref('stg_ga4__events')}}
-    group by 1,2
+    where session_key is not null
+    group by 1
     )
 
 {% endif %}
