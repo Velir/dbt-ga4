0d7dfa0c39ef88769ec0906557b527be8e00e509
Adam Ribaudo <adam.ribaudo@velir.com>
2022-10-29 09:54:07 -0400

    working incremental sessions fact table

--- models/marts/core/core_ga4__sessions.sql
+++ models/marts/core/core_ga4__sessions.sql
@@ -13,7 +13,6 @@
 
 -- TODO: incorporate session conversions. requires refactoring that model to partition on session start date
 
-{% if is_incremental() %}
     with session_metrics as (
         select 
             session_key,
@@ -26,28 +25,29 @@
             sum(engagement_time_msec) as sum_engagement_time_msec
         from {{ref('stg_ga4__events')}}
         -- Give 1 extra day to ensure we beging aggregation at the start of a session
-        where event_date_dt >= DATE_SUB(_dbt_max_partition, INTERVAL 1 DAY)
-        and session_key is not null
+        where session_key is not null
+        {% if is_incremental() %}
+            and event_date_dt >= DATE_SUB(_dbt_max_partition, INTERVAL 1 DAY)
+        {% endif %}
         group by 1,2
+    ),
+    session_conversions as (
+        select * from {{ref('stg_ga4__session_conversions')}}
+        {% if is_incremental() %}
+            where session_start_date >= DATE_SUB(_dbt_max_partition, INTERVAL 1 DAY)
+        {% endif %}
+    ),
+    join_metrics_and_conversions as (
+        select 
+            session_metrics.user_pseudo_id,
+            session_metrics.session_start_timestamp,
+            session_metrics.count_page_views,
+            session_metrics.sum_event_value_in_usd,
+            session_metrics.session_engaged,
+            session_metrics.sum_engagement_time_msec,
+            session_conversions.*
+        from session_metrics left join session_conversions using (session_key)
     )
-{% else %}
 
-    with session_metrics as (
-    select 
-        session_key,
-        user_pseudo_id,
-        min(event_date_dt) as session_start_date,
-        min(event_timestamp) as session_start_timestamp,
-        countif(event_name = 'page_view') as count_page_views,
-        sum(event_value_in_usd) as sum_event_value_in_usd,
-        ifnull(max(session_engaged), 0) as session_engaged,
-        sum(engagement_time_msec) as sum_engagement_time_msec
-    from {{ref('stg_ga4__events')}}
-    where session_key is not null
-    group by 1,2
-    )
-
-{% endif %}
-
-select * from session_metrics
+select * from join_metrics_and_conversions
 
