746e94901cf1be3c5f3d3880e478c1419aab59bb
Will Bryant <will.bryant@gmail.com>
2022-11-03 14:07:52 +1300

    refactor: Simplify sessions conversions queries (#88)
    Similar to #82. To avoid 'Resources exceeded during query execution: Not enough resources for query planning - too many subqueries or query is too complex.' errors.

--- models/staging/ga4/stg_ga4__session_conversions.sql
+++ models/staging/ga4/stg_ga4__session_conversions.sql
@@ -2,45 +2,14 @@
   enabled= var('conversion_events', false) != false
 ) }}
 
-with events as (
-    select 
-        1 as event_count, 
-        session_key, 
-        event_name
-    from {{ref('stg_ga4__events')}}
-),
-sessions as (
-    select 
-        distinct session_key
-    from events
-)
--- For loop that creates 1 cte per conversions, grouped by session key
-{% for ce in var('conversion_events',[]) %}
-,
-conversion_{{ce}} as (
-    select
-        session_key,
-        sum(event_count) as conversion_count
-    from events
-    where event_name = '{{ce}}'
-    group by session_key
-)
-
-{% endfor %}
-
-,
--- Finally, join in each conversion count as a new column
-final_pivot as (
+with event_counts as (
     select 
         session_key
         {% for ce in var('conversion_events',[]) %}
-        , ifnull(conversion_{{ce}}.conversion_count,0) as {{ce}}_count
+        , countif(event_name = '{{ce}}') as {{ce}}_count
         {% endfor %}
-    from sessions
-    {% for ce in var('conversion_events',[]) %}
-    left join conversion_{{ce}} using (session_key)
-    {% endfor %}
+    from {{ref('stg_ga4__events')}}
+    group by session_key
 )
 
-select * from final_pivot
-
+select * from event_counts
