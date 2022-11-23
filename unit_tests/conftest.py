d21b2c6b7463e1d5d4d958b4ecf4a7a853b9a934
Adam Ribaudo <adam.ribaudo@velir.com>
2022-11-22 19:54:29 -0500

    started testing workflow

--- unit_tests/conftest.py
+++ unit_tests/conftest.py
@@ -7,10 +7,20 @@ pytest_plugins = ["dbt.tests.fixtures.project"]
 # The profile dictionary, used to write out profiles.yml
 @pytest.fixture(scope="class")
 def dbt_profile_target():
+    # Set project and keyfile for automated tests
+    if os.environ['GITHUB_ACTIONS'] == True:
+        return {
+            'type': 'bigquery',
+            'method': 'service-account',
+            'keyfile': os.environ.get("DBT_GOOGLE_BIGQUERY_KEYFILE"),
+            'threads': 4,
+            'timeout_seconds': 300,
+            'project':  os.environ.get("BQ_PROJECT")
+        }
     return {
         'type': 'bigquery',
         'method': 'oauth',
-        'threads': 1,
+        'threads': 4,
         'project':  os.environ.get("BQ_PROJECT")
         # project isn't needed if you configure a default, via 'gcloud config set project'
     }
