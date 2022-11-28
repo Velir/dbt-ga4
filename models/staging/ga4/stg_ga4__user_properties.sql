6c34fc2f39ef9162d20d847fb38b4754176fb7c8
Adam Ribaudo <adam.ribaudo@velir.com>
2022-11-27 19:15:19 -0500

    prep for user id mart

--- models/staging/ga4/stg_ga4__user_properties.sql
+++ models/staging/ga4/stg_ga4__user_properties.sql
@@ -3,15 +3,15 @@
   materialized = "table"
 ) }}
 
--- Remove null user_keys (users with privacy enabled)
+-- Remove null user_pseudo_id (users with privacy enabled)
 with events_from_valid_users as (
     select * from {{ref('stg_ga4__events')}}
-    where user_key is not null
+    where user_pseudo_id is not null
 ),
 unnest_user_properties as
 (
     select 
-        user_key,
+        user_pseudo_id,
         event_timestamp
         {% for up in var('user_properties', []) %}
             ,{{ ga4.unnest_key('user_properties',  up.user_property_name ,  up.value_type ) }}
@@ -23,7 +23,7 @@ unnest_user_properties as
 ,non_null_{{up.user_property_name}} as
 (
     select
-        user_key,
+        user_pseudo_id,
         event_timestamp,
         {{up.user_property_name}}
     from unnest_user_properties
@@ -33,34 +33,34 @@ unnest_user_properties as
 last_value_{{up.user_property_name}} as 
 (
     select
-        user_key,
-        LAST_VALUE({{ up.user_property_name }}) OVER (PARTITION BY user_key ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS {{up.user_property_name}}
+        user_pseudo_id,
+        LAST_VALUE({{ up.user_property_name }}) OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS {{up.user_property_name}}
     from non_null_{{up.user_property_name}}
 ),
 last_value_{{up.user_property_name}}_grouped as 
 (
     select
-        user_key,
+        user_pseudo_id,
         {{up.user_property_name}}
     from last_value_{{up.user_property_name}}
-    group by user_key, {{up.user_property_name}}
+    group by user_pseudo_id, {{up.user_property_name}}
 )
 {% endfor %}
 ,
-user_keys as 
+user_pseudo_ids as 
 (
     select distinct
-        user_key
+        user_pseudo_id
     from events_from_valid_users
 ),
 join_properties as 
 (
     select
-        user_key
+        user_pseudo_id
         {% for up in var('user_properties', []) %}
         ,last_value_{{up.user_property_name}}_grouped.{{up.user_property_name}}
         {% endfor %}
-    from user_keys
+    from user_pseudo_ids
     {% for up in var('user_properties', []) %}
     left join last_value_{{up.user_property_name}}_grouped using (user_key)
     {% endfor %}
