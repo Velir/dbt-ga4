9977d274ee8f6fd61b1908ba14b54f02316309a6
Adam Ribaudo <adam.ribaudo@velir.com>
2022-11-27 19:05:14 -0500

    working mapping table and test passed

--- unit_tests/test_stg_Ga4__user_id_mapping.py
+++ unit_tests/test_stg_Ga4__user_id_mapping.py
@@ -2,8 +2,6 @@ import pytest
 from dbt.tests.util import read_file,check_relations_equal,run_dbt
 
 # Define mocks via CSV (seeds) or SQL (models)
-# 1662178591054679 is 2022-09-03
-# 1662091373338190 is 2022-09-02
 mock_stg_ga4__events_csv = """user_pseudo_id,user_id,event_timestamp
 a1,,100
 a1,A,101
@@ -11,13 +9,14 @@ b1,B,102
 c1,C,103
 c2,C,104
 c2,,105
+d1,,100
 """.lstrip()
 
-expected_csv = """user_pseudo_id,last_seen_timestamp,last_seen_user_id
-a1,101,A
-b1,102,B
-c1,103,C
-c2,105,C
+expected_csv = """last_seen_user_id,user_pseudo_id,last_seen_timestamp
+A,a1,101
+B,b1,102
+C,c1,103
+C,c2,104
 """.lstrip()
 
 actual = read_file('../models/staging/ga4/stg_ga4__user_id_mapping.sql')
@@ -39,11 +38,6 @@ class TestUserIdMapping():
         }
     
     def test_mock_run_and_check(self, project):
-        #run_dbt(["build", "--vars", "conversion_events: ['page_view']"])
-        run_dbt(["seed", "-m", "stg_ga4__events"])
-        run_dbt(["seed", "-m", "expected"])
-        run_dbt(["run"])
+        run_dbt(["build"])
         #breakpoint()
         check_relations_equal(project.adapter, ["actual", "expected"])
-
-        # TODO, update mocked events... somehow. Then run again and compare to a new 'expected' table. 
