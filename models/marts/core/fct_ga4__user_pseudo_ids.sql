e8c7df21ccdf2da920c00335b7ea894adfffc124
Adam Ribaudo <adam.ribaudo@velir.com>
2022-11-29 19:46:04 -0500

    fact tables for user ids and user pseudo ids

---
+++ models/marts/core/fct_ga4__user_pseudo_ids.sql
@@ -0,0 +1,16 @@
+select
+    user_pseudo_id,
+    min(session_start_timestamp) as first_seen_timestamp,
+    min(session_start_date) as first_seen_start_date,
+    sum(count_pageviews) as count_pageviews,
+    sum(is_session_engaged) as count_engaged_sessions,
+    sum(sum_event_value_in_usd) as sum_event_value_in_usd,
+    sum(sum_engaged_time_msec) as sum_engaged_time_msec
+    {% if var('conversion_events', false) %}
+        {% for ce in var('conversion_events',[]) %}
+            , sum(count_{{ce}}) as count_{{ce}}
+        {% endfor %}
+    {% endif %}
+from {{ref('fct_ga4__sessions')}}
+group by 1
+
