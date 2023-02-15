5c4f3627d37d3445c138b6196591025d536ec119
Adam Ribaudo <adam.ribaudo@velir.com>
2023-02-15 09:12:07 -0500

    should be >= not >

--- models/staging/ga4/stg_ga4__scroll_90_pct.sql
+++ models/staging/ga4/stg_ga4__scroll_90_pct.sql
@@ -3,7 +3,7 @@ select
     event_date_dt
     , page_location
     , session_key
-    , countif(percent_scrolled >= 90) > 1 as scroll_90_pct --true or false
+    , countif(percent_scrolled >= 90) >= 1 as scroll_90_pct --true or false
  from 
     {{ref('stg_ga4__event_scroll')}}
 group by 1,2,3
