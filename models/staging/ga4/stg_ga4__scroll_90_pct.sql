b571b069494a5d4f75e83e735d2a8590271cb011
Adam Ribaudo <adam.ribaudo@velir.com>
2023-02-15 09:05:46 -0500

    Create stg_ga4__scroll_90_pct.sql

---
+++ models/staging/ga4/stg_ga4__scroll_90_pct.sql
@@ -0,0 +1,9 @@
+-- Determines whether >=90% scroll was reached per date, page_location and session_key
+select
+    event_date_dt
+    , page_location
+    , session_key
+    , countif(percent_scrolled >= 90) > 1 as scroll_90_pct --true or false
+ from 
+    {{ref('stg_ga4__event_scroll')}}
+group by 1,2,3
