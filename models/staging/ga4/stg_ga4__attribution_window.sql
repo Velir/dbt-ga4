5428edb5d0cd4dd59cfef88c48bd8fc9d749f5a2
Damon Gudaitis <dgudaitis@threeventures.com>
2023-02-17 12:02:46 -0800

    fix attribution window excluding nulls

--- models/staging/ga4/stg_ga4__attribution_window.sql
+++ models/staging/ga4/stg_ga4__attribution_window.sql
@@ -1,9 +1,6 @@
 -- This staging model contains key creation and window functions. Keeping window functions outside of the base incremental model ensures that the incremental updates don't artificially limit the window partition sizes (ex: if a session spans 2 days, but only 1 day is in the incremental update)
 {% if not flags.FULL_REFRESH %}
-    {% set partitions_to_query = ['current_date'] %}
-    {% for i in range(var('attribution_window', 30)) %}
-        {% set partitions_to_query = partitions_to_query.append('date_sub(current_date, interval ' + (i+1)|string + ' day)') %}
-    {% endfor %}
+    {% set min_partition_to_query = 'date_sub(current_date, interval '+ (var("attribution_window", 30) + var("static_incremental_days")  ) +' day )' %}
 {% endif %}
 with base_events as (
     select
@@ -19,13 +16,24 @@ with base_events as (
         stream_id,
         ga_session_id
     from {{ ref('base_ga4__events')}}
-    where (source is not null and medium is not null and campaign is not null)
     {% if not flags.FULL_REFRESH %}
-        and event_date_dt in ({{ partitions_to_query | join(',') }})
+        and event_date_dt >= ({{ min_partition_to_query }})
     {% endif %}
     {% if var('frequency', 'daily') == 'daily+streaming' %}
     union all
-    select * from {{ref('base_ga4__events_intraday')}}
+    select
+        user_pseudo_id,
+        event_date_dt,
+        event_timestamp,
+        campaign,
+        source,
+        medium,
+        content,
+        term,
+        event_name,
+        stream_id,
+        ga_session_id
+    from {{ref('base_ga4__events_intraday')}}
     {% endif %}
 ),
 -- Add a unique key for the user that checks for user_id and then pseudo_user_id
