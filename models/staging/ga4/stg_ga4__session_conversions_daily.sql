1edc9fb2a5963a4384508da14e3cebe2f09fd2cd
lowejoshua <lowe@precisdigital.com>
2023-03-03 14:03:14 +0000

    Adding conversion values from var

--- models/staging/ga4/stg_ga4__session_conversions_daily.sql
+++ models/staging/ga4/stg_ga4__session_conversions_daily.sql
@@ -1,6 +1,6 @@
 {{ config(
     enabled= var('conversion_events', false) != false,
-    materialized = 'incremental' if target.name == 'prod' else 'view',
+    materialized = 'incremental',
     incremental_strategy = 'insert_overwrite',
     tags = ["incremental"],
     partition_by={
@@ -15,12 +15,19 @@ with event_counts as (
     select 
         session_key,
         session_partition_key,
+        traffic_source_name,
+        traffic_source_medium,
+        traffic_source_source,
         min(event_date_dt) as session_partition_date -- The date of this partition, not necessarily the session start date given that sessions can span multiple days
         {% for ce in var('conversion_events',[]) %}
         , countif(event_name = '{{ce}}') as {{ce}}_count
         {% endfor %}
+        {% for ce in var('conversion_values', []) %}
+            , SUM(IF(event_name = '{{ce}}', CAST( {{ce}} as FLOAT64), 0)) as {{ce}}_value
+        {% endfor %}
     from {{ref('stg_ga4__events')}}
-    group by 1,2
+    LEFT JOIN  {{ref("stg_ga4__derived_session_properties")}} USING (session_key)
+    group by 1,2,3,4,5
 )
 
 select * from event_counts
