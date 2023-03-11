96d0f27dec77ee93c9a104483d32ab012ddcec03
Adam Ribaudo <adam.ribaudo@velir.com>
2023-03-11 10:14:27 -0500

    more refactoring of event level acquisition fields

--- models/staging/stg_ga4__sessions_traffic_sources.sql
+++ models/staging/stg_ga4__sessions_traffic_sources.sql
@@ -2,14 +2,14 @@ with session_events as (
     select 
         session_key,
         event_timestamp,
-        event_source,
+        events.event_source,
         event_medium,
         event_campaign,
         event_content,
         event_term,
         source_category
-    from {{ref('stg_ga4__events')}}
-    left join {{ref('ga4_source_categories')}} using (source)
+    from {{ref('stg_ga4__events')}} events
+    left join {{ref('ga4_source_categories')}} source_categories on events.event_source = source_categories.source
     where session_key is not null
     and event_name != 'session_start'
     and event_name != 'first_visit'
@@ -23,13 +23,13 @@ set_default_channel_grouping as (
 session_source as (
     select    
         session_key,
-        COALESCE(FIRST_VALUE((CASE WHEN source <> '(direct)' THEN source END) IGNORE NULLS) OVER (session_window), '(direct)') AS session_source,
-        COALESCE(FIRST_VALUE((CASE WHEN source <> '(direct)' THEN COALESCE(event_medium, '(none)') END) IGNORE NULLS) OVER (session_window), '(none)') AS session_medium,
-        COALESCE(FIRST_VALUE((CASE WHEN source <> '(direct)' THEN COALESCE(source_category, '(none)') END) IGNORE NULLS) OVER (session_window), '(none)') AS source_category,
-        COALESCE(FIRST_VALUE((CASE WHEN source <> '(direct)' THEN COALESCE(event_campaign, '(none)') END) IGNORE NULLS) OVER (session_window), '(none)') AS session_campaign,
-        COALESCE(FIRST_VALUE((CASE WHEN source <> '(direct)' THEN COALESCE(event_content, '(none)') END) IGNORE NULLS) OVER (session_window), '(none)') AS session_content,
-        COALESCE(FIRST_VALUE((CASE WHEN source <> '(direct)' THEN COALESCE(event_term, '(none)') END) IGNORE NULLS) OVER (session_window), '(none)') AS session_term,
-        COALESCE(FIRST_VALUE((CASE WHEN source <> '(direct)' THEN COALESCE(default_channel_grouping, '(none)') END) IGNORE NULLS) OVER (session_window), '(none)') AS default_channel_grouping
+        COALESCE(FIRST_VALUE((CASE WHEN event_source <> '(direct)' THEN event_source END) IGNORE NULLS) OVER (session_window), '(direct)') AS session_source,
+        COALESCE(FIRST_VALUE((CASE WHEN event_source <> '(direct)' THEN COALESCE(event_medium, '(none)') END) IGNORE NULLS) OVER (session_window), '(none)') AS session_medium,
+        COALESCE(FIRST_VALUE((CASE WHEN event_source <> '(direct)' THEN COALESCE(source_category, '(none)') END) IGNORE NULLS) OVER (session_window), '(none)') AS source_category,
+        COALESCE(FIRST_VALUE((CASE WHEN event_source <> '(direct)' THEN COALESCE(event_campaign, '(none)') END) IGNORE NULLS) OVER (session_window), '(none)') AS session_campaign,
+        COALESCE(FIRST_VALUE((CASE WHEN event_source <> '(direct)' THEN COALESCE(event_content, '(none)') END) IGNORE NULLS) OVER (session_window), '(none)') AS session_content,
+        COALESCE(FIRST_VALUE((CASE WHEN event_source <> '(direct)' THEN COALESCE(event_term, '(none)') END) IGNORE NULLS) OVER (session_window), '(none)') AS session_term,
+        COALESCE(FIRST_VALUE((CASE WHEN event_source <> '(direct)' THEN COALESCE(default_channel_grouping, '(none)') END) IGNORE NULLS) OVER (session_window), '(none)') AS default_channel_grouping
     from set_default_channel_grouping
     WINDOW session_window AS (PARTITION BY session_key ORDER BY event_timestamp ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
 )
