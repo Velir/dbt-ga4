bfadffd56a9a0d5e5da8b39e011c9229ce837e20
Adam Ribaudo <adam.ribaudo@velir.com>
2023-03-11 20:09:15 -0500

    incremental run can handle if there are missing shards

--- macros/combine_property_data.sql
+++ macros/combine_property_data.sql
@@ -2,41 +2,32 @@
 
     create schema if not exists `{{var('project')}}.{{var('dataset')}}`;
 
-    -- Use static_incremental_days if is_incremental, otherwise pull all shards
+    -- If is_incremental, then use static_incremental_days variable to find earliest shard to copy
     {% if is_incremental() %}
-        {% set shards_to_retrieve = [] %}
-        {% for i in range(var('static_incremental_days')) %}
-            -- use modules.datetime to get current date, subtract N days, remove dashes in the date
-            -- for now, starting with 2 days ago because I'm working with a client where the data appears late
-            {% 
-                set shards_to_retrieve = shards_to_retrieve.append(
-                    (modules.datetime.date.today() - modules.datetime.timedelta(days=i+2))|string|replace("-", "")
-                ) 
-            %}
-        {% endfor %}
+        {% set earliest_shard_to_retrieve = (modules.datetime.date.today() - modules.datetime.timedelta(days=var('static_incremental_days')))|string|replace("-", "") %}
         
-        -- For each date and each property id, copy table to target dataset
-        {% for shard in shards_to_retrieve %}
-            {% for property_id in var('property_ids') %}
-                CREATE OR REPLACE TABLE `{{var('project')}}.{{var('dataset')}}.events_{{shard}}{{property_id}}` CLONE `{{var('project')}}.analytics_{{property_id}}.events_{{shard}}`;
-            {% endfor %}
-        {% endfor %}
-
-    -- Else is full refresh
-    {% else %}
-        {% for property_id in var('property_ids') %}
-            {% set schema_name = "analytics_" + property_id|string %}
-            {% set relations = dbt_utils.get_relations_by_pattern(schema_name, 'events_%') %}
+        {%- for property_id in var('property_ids') -%}
+            {%- set schema_name = "analytics_" + property_id|string -%}
+            {%- set relations = dbt_utils.get_relations_by_pattern(schema_name, 'events_%') -%}
             {% for relation in relations %}
-                {{ log(relation.identifier) }}
-                {% set relation_suffix = relation.identifier|replace('events_', '') %}
-
-                -- filter relations to those > start_date
-                {% if relation_suffix|int >= var('start_date')|int %}
+                {%- set relation_suffix = relation.identifier|replace('events_', '') -%}
+                {%- if relation_suffix|int >= earliest_shard_to_retrieve|int -%}
                     CREATE OR REPLACE TABLE `{{var('project')}}.{{var('dataset')}}.events_{{relation_suffix}}{{property_id}}` CLONE `{{var('project')}}.analytics_{{property_id}}.events_{{relation_suffix}}`;
-                {% endif %}
+                {%- endif -%}
             {% endfor %}
-        {% endfor %}
+        {%- endfor -%}
+    -- Else is full refresh. Filter to only source tables with a suffix > start_date
+    {% else %}
+        {%- for property_id in var('property_ids') -%}
+            {%- set schema_name = "analytics_" + property_id|string -%}
+            {%- set relations = dbt_utils.get_relations_by_pattern(schema_name, 'events_%') -%}
+            {% for relation in relations %}
+                {%- set relation_suffix = relation.identifier|replace('events_', '') -%}
+                {%- if relation_suffix|int >= var('start_date')|int -%}
+                    CREATE OR REPLACE TABLE `{{var('project')}}.{{var('dataset')}}.events_{{relation_suffix}}{{property_id}}` CLONE `{{var('project')}}.analytics_{{property_id}}.events_{{relation_suffix}}`;
+                {%- endif -%}
+            {% endfor %}
+        {%- endfor -%}
     {% endif %}
 
 {% endmacro %}
