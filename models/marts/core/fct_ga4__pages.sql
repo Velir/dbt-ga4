adcae7c11f1b5b7e995c130c1bb3c124984ec232
dgitis <damon@caretjuice.com>
2023-03-20 13:28:04 -0700

    fix merge artifacts and remove hour from pages mart

--- models/marts/core/fct_ga4__pages.sql
+++ models/marts/core/fct_ga4__pages.sql
@@ -15,7 +15,6 @@ with page_view as (
     select
         event_date_dt,
         stream_id,
-        extract( hour from (select  timestamp_micros(event_timestamp))) as hour,
         page_location,  -- includes query string parameters not listed in query_parameter_exclusions variable
         page_key,
         page_path,
@@ -27,7 +26,6 @@ with page_view as (
         sum(entrances) as entrances,
 from {{ref('stg_ga4__event_page_view')}}
     group by 1,2,3,4,5,6,7
-<<<<<<< HEAD
 ), page_engagement as (
     select
         page_view.* except(page_engagement_key),
@@ -36,17 +34,14 @@ from {{ref('stg_ga4__event_page_view')}}
     from {{ ref('stg_ga4__page_engaged_time') }}
     right join page_view using (page_engagement_key)
     group by 1,2,3,4,5,6,7,8,9,10
-=======
->>>>>>> b07ace274454594c831b40195f8b54d7b64800d5
 ), scroll as (
     select
         event_date_dt,
-        extract( hour from (select timestamp_micros(event_timestamp))) as hour,
         page_location, 
         page_title,
         count(event_name) as scroll_events
     from {{ref('stg_ga4__event_scroll')}}
-    group by 1,2,3,4
+    group by 1,2,3
 )
 {% if var('conversion_events',false) %}
 ,
@@ -60,11 +55,11 @@ select
     join_conversions.*  except (page_key),
     ifnull(scroll.scroll_events, 0) as scroll_events
 from join_conversions
-left join scroll using (event_date_dt, hour, page_location, page_title)
+left join scroll using (event_date_dt, page_location, page_title)
 {% else %}
 select
     page_engagement.* except (page_key),
     ifnull(scroll.scroll_events, 0) as scroll_events
 from page_engagement
-left join scroll using (event_date_dt, hour, page_location, page_title)
+left join scroll using (event_date_dt, page_location, page_title)
 {% endif %}
