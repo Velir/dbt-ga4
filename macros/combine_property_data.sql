0c3142a52b493d0bc3eb883103b48d5eca2fde9b
Damon Gudaitis <damon@caretjuice.com>
2023-04-22 06:13:07 -0700

    multi-site support for streaming frequency (#187)
    * multi-site support for streaming frequency
    
    * tweaking documentation
    
    ---------
    
    Co-authored-by: Adam Ribaudo <adam.ribaudo@velir.com>

--- macros/combine_property_data.sql
+++ macros/combine_property_data.sql
@@ -16,13 +16,22 @@
 
     {% for property_id in var('property_ids') %}
         {%- set schema_name = "analytics_" + property_id|string -%}
-        {%- set relations = dbt_utils.get_relations_by_pattern(schema_pattern=schema_name, table_pattern='events_%', exclude='events_intraday_%', database=var('project')) -%}
-        {% for relation in relations %}
-            {%- set relation_suffix = relation.identifier|replace('events_', '') -%}
-            {%- if relation_suffix|int >= earliest_shard_to_retrieve|int -%}
-                CREATE OR REPLACE TABLE `{{var('project')}}.{{var('dataset')}}.events_{{relation_suffix}}{{property_id}}` CLONE `{{var('project')}}.analytics_{{property_id}}.events_{{relation_suffix}}`;
-            {%- endif -%}
-        {% endfor %}
+        {%- if var('frequency', daily) == 'streaming' -%}
+            {%- set relations = dbt_utils.get_relations_by_pattern(schema_pattern=schema_name, table_pattern='events_intraday_%', database=var('project')) -%}
+            {% for relation in relations %}
+                {%- set relation_suffix = relation.identifier|replace('events_intraday_', '') -%}
+                {%- if relation_suffix|int >= earliest_shard_to_retrieve|int -%}
+                    CREATE OR REPLACE TABLE `{{var('project')}}.{{var('dataset')}}.events_intraday_{{relation_suffix}}{{property_id}}` CLONE `{{var('project')}}.analytics_{{property_id}}.events_intraday_{{relation_suffix}}`;
+                {%- endif -%}
+            {% endfor %}
+        {%- else -%}
+            {%- set relations = dbt_utils.get_relations_by_pattern(schema_pattern=schema_name, table_pattern='events_%', exclude='events_intraday_%', database=var('project')) -%}
+            {% for relation in relations %}
+                {%- set relation_suffix = relation.identifier|replace('events_', '') -%}
+                {%- if relation_suffix|int >= earliest_shard_to_retrieve|int -%}
+                    CREATE OR REPLACE TABLE `{{var('project')}}.{{var('dataset')}}.events_{{relation_suffix}}{{property_id}}` CLONE `{{var('project')}}.analytics_{{property_id}}.events_{{relation_suffix}}`;
+                {%- endif -%}
+            {% endfor %}
+        {%- endif -%}
     {% endfor %}
-
 {% endmacro %}
