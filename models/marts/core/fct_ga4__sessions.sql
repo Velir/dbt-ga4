32ca44bc075b55c73f7f5ea850639e3906bfc3ad
Adam Ribaudo <adam.ribaudo@velir.com>
2023-04-27 06:56:44 -0400

    fix col names and build errors

--- models/marts/core/fct_ga4__sessions.sql
+++ models/marts/core/fct_ga4__sessions.sql
@@ -4,7 +4,7 @@ select
     client_key,
     session_key,
     stream_id,
-    max(user_id),
+    max(user_id) as user_id,
     min(session_partition_min_timestamp) as session_start_timestamp,
     min(session_partition_date) as session_start_date,
     sum(session_partition_count_page_views) as count_pageviews,
@@ -12,12 +12,12 @@ select
     sum(session_partition_sum_event_value_in_usd) as sum_event_value_in_usd,
     max(session_partition_max_session_engaged) as is_session_engaged,
     sum(session_partition_sum_engagement_time_msec) as sum_engaged_time_msec,
-    min(ga_session_number) as ga_session_number
+    min(session_number) as session_number
     {% if var('conversion_events', false) %}
         {% for ce in var('conversion_events',[]) %}
             , sum({{ce}}_count) as count_{{ce}}
         {% endfor %}
     {% endif %}
 from {{ref('fct_ga4__sessions_daily')}}
-group by 1,2,3,4
+group by 1,2,3
 
--- models/marts/core/fct_ga4__sessions_daily.sql
+++ models/marts/core/fct_ga4__sessions_daily.sql
@@ -45,7 +45,7 @@ with session_metrics as (
         sum(event_value_in_usd) as session_partition_sum_event_value_in_usd,
         ifnull(max(session_engaged), 0) as session_partition_max_session_engaged,
         sum(engagement_time_msec) as session_partition_sum_engagement_time_msec,
-        min(ga_session_number) as ga_session_number
+        min(session_number) as session_number
     from {{ref('stg_ga4__events')}}
     where session_key is not null
     {% if is_incremental() %}
@@ -76,12 +76,14 @@ with session_metrics as (
         select 
             session_metrics.client_key,
             session_metrics.stream_id,
+            session_metrics.user_id,
             session_metrics.session_partition_min_timestamp,
             session_metrics.session_partition_count_page_views,
             session_metrics.session_partition_count_purchases,
             session_metrics.session_partition_sum_event_value_in_usd,
             session_metrics.session_partition_max_session_engaged,
             session_metrics.session_partition_sum_engagement_time_msec,
+            session_metrics.session_number,
             session_conversions.*
         from session_metrics left join session_conversions using (session_partition_key)
     )
