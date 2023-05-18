bd4b384069aac860bc3dc1c445a85aa3e4de136e
YutaNakamura <38100988+YutaNakamura1413@users.noreply.github.com>
2023-05-19 01:26:48 +0900

    format conversion_event_column_name

--- macros/conversion_event_column_name.sql
+++ macros/conversion_event_column_name.sql
@@ -1,7 +1,7 @@
 {% macro conversion_event_column_name(event_name, prefix, suffix) %}
-  {% if var('conversion_event_renamers', false) and var('conversion_event_renamers')[event_name] %}
-    {{prefix}}{{var('conversion_event_renamers')[event_name]}}{{suffix}}
-  {% else %}
-    {{prefix}}{{event_name}}{{suffix}}
-  {% endif %}
+    {% if var('conversion_event_renamers', false) and var('conversion_event_renamers')[event_name] %}
+        {{prefix}}{{var('conversion_event_renamers')[event_name]}}{{suffix}}
+    {% else %}
+        {{prefix}}{{event_name}}{{suffix}}
+    {% endif %}
 {% endmacro %}
