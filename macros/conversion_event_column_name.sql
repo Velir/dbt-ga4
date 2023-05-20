4808f0b4424c1fcef91b60819ac2c406826ee54b
YutaNakamura <38100988+YutaNakamura1413@users.noreply.github.com>
2023-05-20 14:21:33 +0900

    fix variable name

--- macros/conversion_event_column_name.sql
+++ macros/conversion_event_column_name.sql
@@ -1,6 +1,6 @@
 {% macro conversion_event_column_name(event_name, prefix, suffix) %}
-    {% if var('conversion_event_renamers', false) and var('conversion_event_renamers')[event_name] %}
-        {{prefix}}{{var('conversion_event_renamers')[event_name]}}{{suffix}}
+    {% if var('conversion_event_column_renamers', false) and var('conversion_event_column_renamers')[event_name] %}
+        {{prefix}}{{var('conversion_event_column_renamers')[event_name]}}{{suffix}}
     {% else %}
         {{prefix}}{{event_name}}{{suffix}}
     {% endif %}
