af6b3815977db5462c2c2888c6f5bcfa31cb5284
Damon Gudaitis <damon@caretjuice.com>
2023-06-05 04:21:27 -0700

    fix code getting accidentally commented out (#228)
    * multi-site support for streaming frequency
    
    * tweaking documentation
    
    * fix code getting accidentally commented out
    
    * removing jinja tags that remove whitespace
    
    ---------
    
    Co-authored-by: Adam Ribaudo <adam.ribaudo@velir.com>

--- macros/combine_property_data.sql
+++ macros/combine_property_data.sql
@@ -7,12 +7,13 @@
     create schema if not exists `{{var('project')}}.{{var('dataset')}}`;
 
     -- If incremental, then use static_incremental_days variable to find earliest shard to copy
-    {%- if not should_full_refresh() -%}
+    {% if not should_full_refresh() %}
         {% set earliest_shard_to_retrieve = (modules.datetime.date.today() - modules.datetime.timedelta(days=var('static_incremental_days')))|string|replace("-", "")|int %}
-    {%- else -%}
+    {% else %}
     -- Otherwise use 'start_date' variable
+
         {% set earliest_shard_to_retrieve = var('start_date')|int %}
-    {%- endif -%}
+    {% endif %}
 
     {% for property_id in var('property_ids') %}
         {%- set schema_name = "analytics_" + property_id|string -%}
