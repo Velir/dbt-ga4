5951dd494b8d03883e939184b08f2002825cfee2
Adam Ribaudo <adam.ribaudo@velir.com>
2023-06-30 06:46:02 -0400

    update formatting, paid search, organic social, paid social

--- macros/default_channel_grouping.sql
+++ macros/default_channel_grouping.sql
@@ -14,43 +14,64 @@ case
     and ({{medium}} = '(none)' or {{medium}} = '(not set)')
     then 'Direct'
 
-  when REGEXP_CONTAINS({{source}}, r"^(facebook|instagram|pinterest|reddit|twitter|linkedin)") = true
-    and REGEXP_CONTAINS({{medium}}, r"^(cpc|ppc|paid)") = true
+  when 
+    (
+      REGEXP_CONTAINS({{source}}, r"^(facebook|instagram|pinterest|reddit|twitter|linkedin)") = true
+      or {{source_category}} = 'SOURCE_CATEGORY_SOCIAL' 
+    )
+      and REGEXP_CONTAINS({{medium}}, r"^(.*cp.*|ppc|retargeting|paid.*)$") = true
     then 'Paid Social'
-  when REGEXP_CONTAINS({{source}}, r"^(facebook|instagram|pinterest|reddit|twitter|linkedin)") = true
-    or REGEXP_CONTAINS({{medium}}, r"^(social|social-network|social-media|sm|social network|social media)") = true
-    or {{source_category}} = 'SOURCE_CATEGORY_SOCIAL' 
+  when 
+    REGEXP_CONTAINS({{source}}, r"^(facebook|instagram|pinterest|reddit|twitter|linkedin)") = true
+      or {{medium}} in ("social","social-network","social-media","sm","social network","social media")
+      or {{source_category}} = 'SOURCE_CATEGORY_SOCIAL' 
     then 'Organic Social'
-  when REGEXP_CONTAINS({{medium}}, r"email|e-mail|e_mail|e mail") = true
-    or REGEXP_CONTAINS({{source}}, r"email|e-mail|e_mail|e mail") = true
+  when 
+    REGEXP_CONTAINS({{medium}}, r"email|e-mail|e_mail|e mail") = true
+      or REGEXP_CONTAINS({{source}}, r"email|e-mail|e_mail|e mail") = true
     then 'Email'
-  when REGEXP_CONTAINS({{medium}}, r"affiliate|affiliates") = true
+  when 
+    REGEXP_CONTAINS({{medium}}, r"affiliate|affiliates") = true
     then 'Affiliates'
-  when {{source_category}} = 'SOURCE_CATEGORY_SHOPPING' and REGEXP_CONTAINS({{medium}},r"^(.*cp.*|ppc|paid.*)$")
+  when 
+    {{source_category}} = 'SOURCE_CATEGORY_SHOPPING' 
+      and REGEXP_CONTAINS({{medium}},r"^(.*cp.*|ppc|paid.*)$")
     then 'Paid Shopping'
-  when ({{source_category}} = 'SOURCE_CATEGORY_VIDEO' AND REGEXP_CONTAINS({{medium}},r"^(.*cp.*|ppc|paid.*)$"))
-    or {{source}} = 'dv360_video'
+  when 
+    ({{source_category}} = 'SOURCE_CATEGORY_VIDEO' AND REGEXP_CONTAINS({{medium}},r"^(.*cp.*|ppc|paid.*)$"))
+      or {{source}} = 'dv360_video'
     then 'Paid Video'
-  when REGEXP_CONTAINS({{medium}}, r"^(display|cpm|banner)$")
-    or {{source}} = 'dv360_display'
+  when 
+    REGEXP_CONTAINS({{medium}}, r"^(display|cpm|banner)$")
+      or {{source}} = 'dv360_display'
     then 'Display'
-  when REGEXP_CONTAINS({{medium}}, r"^(cpc|ppc|paidsearch)$")
+  when 
+    {{source_category}} = 'SOURCE_CATEGORY_SEARCH'
+      and REGEXP_CONTAINS({{medium}}, r"^(.*cp.*|ppc|retargeting|paid.*)$")
     then 'Paid Search'
-  when REGEXP_CONTAINS({{medium}}, r"^(cpv|cpa|cpp|content-text)$")
+  when 
+    REGEXP_CONTAINS({{medium}}, r"^(cpv|cpa|cpp|content-text)$")
     then 'Other Advertising'
-  when {{medium}} = 'organic' or {{source_category}} = 'SOURCE_CATEGORY_SEARCH'
+  when 
+    {{medium}} = 'organic' or {{source_category}} = 'SOURCE_CATEGORY_SEARCH'
     then 'Organic Search'
-  when REGEXP_CONTAINS({{medium}}, r"^(.*video.*)$") or {{source_category}} = 'SOURCE_CATEGORY_VIDEO'
+  when 
+    REGEXP_CONTAINS({{medium}}, r"^(.*video.*)$") or {{source_category}} = 'SOURCE_CATEGORY_VIDEO'
     then 'Organic Video'
-  when {{source_category}} = 'SOURCE_CATEGORY_SHOPPING'
+  when 
+    {{source_category}} = 'SOURCE_CATEGORY_SHOPPING'
     then 'Organic Shopping'
-  when {{medium}} = 'referral'
+  when 
+    {{medium}} = 'referral'
     then 'Referral'
-  when {{medium}} = 'audio'
+  when 
+    {{medium}} = 'audio'
     then 'Audio'
-  when {{medium}} = 'sms'
+  when 
+    {{medium}} = 'sms'
     then 'SMS'
-  when REGEXP_CONTAINS({{medium}}, r"(mobile|notification|push$)") or {{source}} = 'firebase'
+  when 
+    REGEXP_CONTAINS({{medium}}, r"(mobile|notification|push$)") or {{source}} = 'firebase'
     then 'Push Notifications'
   else '(Other)' 
 end 
