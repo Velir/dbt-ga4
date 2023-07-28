52b37bc973d9c852338bf3eb27e4b3852592b539
Juliana de Melo <julianademelo@MacBook-Pro.local>
2023-07-28 12:33:11 +0200

    Add map_stream_id macro

---
+++ macros/map_stream_id.sql
@@ -0,0 +1,8 @@
+{% macro map_stream_id(stream_id, stream_properties) -%}
+CASE
+{% for sp in stream_properties %}
+    WHEN {{ stream_id }} = {{ sp.stream_id }} THEN {{ sp.stream_name }}
+{% endfor %}
+	ELSE "new_stream"
+END
+{% endmacro -%}
