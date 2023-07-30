6a428ae8ea50ad0985f165d6dce814653f7fbe05
Adam Ribaudo <adam.ribaudo@velir.com>
2023-07-30 16:08:30 -0400

    include intraday data in the source select statement

--- models/staging/base/base_ga4__events.sql
+++ models/staging/base/base_ga4__events.sql
@@ -2,9 +2,7 @@
 {% for i in range(var('static_incremental_days')) %}
     {% set partitions_to_replace = partitions_to_replace.append('date_sub(current_date, interval ' + (i+1)|string + ' day)') %}
 {% endfor %}
-{% if var('property_ids', false) == false %}
-    {% set relations_intraday = dbt_utils.get_relations_by_pattern(schema_pattern=var('dataset'), table_pattern='events_intraday_%', database=var('project')) %} 
-{% endif %}
+
 {{
     config(
         pre_hook="{{ ga4.combine_property_data() }}" if var('property_ids', false) else "",
@@ -19,44 +17,20 @@
     )
 }}
 
-with source_daily as (
+with source as (
     select 
         {{ ga4.base_select_source() }}
         from {{ source('ga4', 'events') }}
-        where _table_suffix not like '%intraday%'
-        and cast( _table_suffix as int64) >= {{var('start_date')}}
+        where cast( replace(_table_suffix, 'intraday_', '') as int64) >= {{var('start_date')}}
     {% if is_incremental() %}
         and parse_date('%Y%m%d', left(_TABLE_SUFFIX, 8)) in ({{ partitions_to_replace | join(',') }})
     {% endif %}
 ),
--- Include intraday data if using a single-property configuration and the events_intraday_* table exists 
-{% if var('property_ids', false) == false and relations_intraday|length > 0 %}
-    source_intraday as (
-        select 
-            {{ ga4.base_select_source() }}
-            from {{ source('ga4', 'events_intraday') }}
-            where cast( _table_suffix as int64) >= {{var('start_date')}}
-        {% if is_incremental() %}
-            and parse_date('%Y%m%d', left(_TABLE_SUFFIX, 8)) in ({{ partitions_to_replace | join(',') }})
-        {% endif %}
-    ),
-    unioned as (
-        select * from source_daily
-            union all
-        select * from source_intraday
-    ),
-    renamed as (
-        select 
-            {{ ga4.base_select_renamed() }}
-        from unioned
-    )
-{% else %}
-    renamed as (
-        select 
-            {{ ga4.base_select_renamed() }}
-        from source_daily
-    )
-{% endif%}
+renamed as (
+    select 
+        {{ ga4.base_select_renamed() }}
+    from source
+)
 
 select * from renamed
 qualify row_number() over(partition by event_date_dt, stream_id, user_pseudo_id, session_id, event_name, event_timestamp, to_json_string(event_params)) = 1
