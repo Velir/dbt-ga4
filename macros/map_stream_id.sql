d920d19fb25578bf2ef9c799baf0f652427efdbe
Juliana de Melo <juliana.de.melo@wellsterhealth.com>
2023-08-01 17:07:52 +0200

    Add double quotes to use values as STRING

--- macros/map_stream_id.sql
+++ macros/map_stream_id.sql
@@ -1,7 +1,7 @@
 {% macro map_stream_id(stream_id, stream_properties) -%}
 CASE
 {% for sp in stream_properties %}
-    WHEN {{ stream_id }} = {{ sp.stream_id }} THEN {{ sp.stream_name }}
+    WHEN {{ stream_id }} = "{{ sp.stream_id }}" THEN "{{ sp.stream_name }}"
 {% endfor %}
 	ELSE "new_stream"
 END
