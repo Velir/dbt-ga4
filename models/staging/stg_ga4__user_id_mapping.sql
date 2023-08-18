ea4851901b8b4d5aaeae67c5ae6e2016c04df1b0
Juliana de Melo <juliana.de.melo@wellsterhealth.com>
2023-08-18 14:30:48 +0200

    Add event_timestamp_dt column

--- models/staging/stg_ga4__user_id_mapping.sql
+++ models/staging/stg_ga4__user_id_mapping.sql
@@ -2,7 +2,8 @@ with events_with_user_id as (
     select 
         user_id,
         client_key,
-        event_timestamp 
+        event_timestamp,
+        event_timestamp_dt
     from {{ref('stg_ga4__events')}}
     where user_id is not null
         and client_key is not null
@@ -11,7 +12,8 @@ include_last_seen_timestamp as (
     select 
         user_id,
         client_key,
-        max(event_timestamp) as last_seen_user_id_timestamp
+        max(event_timestamp) as last_seen_user_id_timestamp,
+        max(event_timestamp_dt) as last_seen_user_id_timestamp_dt
     from events_with_user_id
     group by 1,2
 ),
@@ -19,7 +21,8 @@ pick_latest_timestamp as (
     select
         user_id as last_seen_user_id,
         client_key,
-        last_seen_user_id_timestamp
+        last_seen_user_id_timestamp,
+        last_seen_user_id_timestamp_dt
     from include_last_seen_timestamp
     -- Find the latest mapping between client_key and user_id
     qualify row_number() over(partition by client_key order by last_seen_user_id_timestamp desc) = 1
