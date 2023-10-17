f14f8a10d7cbb818afcf82a53a91ecc993486be6
dgitis <damon@caretjuice.com>
2023-10-17 13:40:27 -0700

    working copy

--- models/staging/recommended_events/stg_ga4__event_purchase_deduplicated.sql
+++ models/staging/recommended_events/stg_ga4__event_purchase_deduplicated.sql
@@ -12,7 +12,7 @@ with purch as (
     /*  this is intended to be the maximally performant MVP for transaction deduplication
         it is possible that you may want to roll up various purchase parameters and not just event_key where later events contain late-arriving parameters
         in cases like this, use this model as a template and make your customizations in your project */ 
-    select
+    select distinct
         first_value(event_key ignore nulls) over (transaction_window) as event_key   
     from purch
     window transaction_window as (
