a53174e14b5dd58532b76e67e9f851a70933bb60
Adam Ribaudo <adam.ribaudo@velir.com>
2023-12-29 08:04:12 -0500

    conditionally apply prehook

--- models/staging/base/base_ga4__events.sql
+++ models/staging/base/base_ga4__events.sql
@@ -5,7 +5,7 @@
 
 {{
     config(
-        pre_hook="{{ ga4.combine_property_data() }}" if var('property_ids', false) else "",
+        pre_hook="{{ ga4.combine_property_data() }}" if var('combined_dataset', false) else "",
         materialized = 'incremental',
         incremental_strategy = 'insert_overwrite',
         partition_by={
