eca70fa81f3da22063446ebbab7d0e9bb89ed31b
yamotech <nothings.2c9@gmail.com>
2024-02-27 22:25:23 +0900

    Fix to filter based on dates equal to or later than the start_date

--- models/staging/base/base_ga4__events.sql
+++ models/staging/base/base_ga4__events.sql
@@ -21,7 +21,7 @@ with source as (
     select
         {{ ga4.base_select_source() }}
     from {{ source('ga4', 'events') }}
-    where cast(replace(_table_suffix, 'intraday_', '') as int64) >= {{var('start_date')}}
+    where cast(left(replace(_table_suffix, 'intraday_', ''), 8) as int64) >= {{var('start_date')}}
     {% if is_incremental() %}
         and parse_date('%Y%m%d', left(replace(_table_suffix, 'intraday_', ''), 8)) in ({{ partitions_to_replace | join(',') }})
     {% endif %}
