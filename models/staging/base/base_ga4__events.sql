151204b7d966948849e036e19f5d7fe0a09c5d2c
Miron Kremenetskiy <81591301+miron-kremenetskiy@users.noreply.github.com>
2024-03-22 15:46:29 -0500

    replace unnest

--- models/staging/base/base_ga4__events.sql
+++ models/staging/base/base_ga4__events.sql
@@ -42,12 +42,30 @@ with source as (
         and parse_date('%Y%m%d', left(replace(_table_suffix, 'intraday_', ''), 8)) in ({{ partitions_to_replace | join(',') }})
     {% endif %}
     and cast(left(replace(_table_suffix, 'intraday_', ''), 8) as int64) = 20240305
-),
-renamed as (
+)
+
+
+{%- if target.type == 'bigquery' -%}
+
     select
         {{ ga4.base_select_renamed() }}
     from source
+    qualify row_number() over(partition by event_date_dt, stream_id, user_pseudo_id, session_id, event_name, event_timestamp, to_json_string(ARRAY(SELECT params FROM UNNEST(event_params) AS params ORDER BY key))) = 1
+
+{%- elif target.type == 'snowflake' -%}
+
+, renamed as (
+
+    select distinct
+        renamed.*
+        , array_agg(e.value) within group(order by e.value) over(partition by e.seq) as event_params_constructed
+    from renamed
+        , lateral flatten(input=>event_params) as e
+
 )
 
-select * from renamed
-qualify row_number() over(partition by event_date_dt, stream_id, user_pseudo_id, session_id, event_name, event_timestamp, to_json_string(ARRAY(SELECT params FROM UNNEST(event_params) AS params ORDER BY key))) = 1
+select *
+from renamed
+qualify row_number() over(partition by event_date_dt, stream_id, user_pseudo_id, session_id, event_name, event_timestamp, event_params_constructed) = 1
+
+{%- endif -%}
