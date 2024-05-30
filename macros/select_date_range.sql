43ca797faae797bcf73b3c32e59c98767350c4a0
JumboDVDH0 <davy.vanderhorst@jumbo.com>
2024-05-30 18:46:38 +0200

    Moved macro to our own project

--- macros/select_date_range.sql
+++
@@ -1,7 +0,0 @@
-{% macro select_date_range(start_date, end_date, date_column) %}
-    {% if start_date is not none and end_date is not none %}
-        CAST({{ date_column }} AS DATE) BETWEEN DATE '{{ start_date }}' AND DATE '{{ end_date }}'
-    {% else %}
-        CAST({{ date_column }} AS DATE) >= CURRENT_DATE() - INTERVAL {{ var("lookback_window") }} DAY
-    {% endif %}
-{% endmacro %}
