5282061e2f90959f85270f0b55f5a4a1675adcf8
Jérôme LAURENT <jerome.laurent.pro@gmail.com>
2024-06-05 15:07:50 +0200

    Fix invalid escape character

--- macros/valid_column_name.sql
+++ macros/valid_column_name.sql
@@ -4,7 +4,7 @@
   {# a column name can't contain a non alphanumeric or _ character #}
   {% set cleaned_name = re.sub(pattern, '_', column_name|string) %}
 
-  {% if re.match('^\d', cleaned_name) %}
+  {% if re.match('^\\d', cleaned_name) %}
     {# a column name can't start by a number #}
     {{ return("_" ~ cleaned_name) }}
   {% else %}
