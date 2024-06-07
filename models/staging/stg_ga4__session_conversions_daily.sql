298f3738cebbf6fe0c804b74ad517de8dc623872
Adam Ribaudo <adam.ribaudo@velir.com>
2024-06-07 07:39:53 -0400

    refactored unit tests for stg_ga4__session_conversions_daily

--- models/staging/stg_ga4__session_conversions_daily.sql
+++ models/staging/stg_ga4__session_conversions_daily.sql
@@ -1,7 +1,11 @@
 {% set partitions_to_replace = ['current_date'] %}
+
+{% if is_incremental() %}
 {% for i in range(var('static_incremental_days')) %}
     {% set partitions_to_replace = partitions_to_replace.append('date_sub(current_date, interval ' + (i+1)|string + ' day)') %}
 {% endfor %}
+{% endif %}
+
 {{
     config(
         enabled= var('conversion_events', false) != false,
