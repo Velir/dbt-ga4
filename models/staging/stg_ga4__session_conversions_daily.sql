de1cdb3583994c7eec4727f76bdcf73f47433c38
JumboDVDH0 <davy.vanderhorst@jumbo.com>
2024-06-26 09:00:13 +0200

    Added backfill compatibility start and end date

--- models/staging/stg_ga4__session_conversions_daily.sql
+++ models/staging/stg_ga4__session_conversions_daily.sql
@@ -2,6 +2,36 @@
 {% for i in range(var('static_incremental_days')) %}
     {% set partitions_to_replace = partitions_to_replace.append('date_sub(current_date, interval ' + (i+1)|string + ' day)') %}
 {% endfor %}
+
+
+{% set start_date = var('start_date', none) %}
+{% set end_date = var('end_date', none) %}
+
+{{ log("Initial start_date: " ~ start_date, info=True) }}
+{{ log("Initial end_date: " ~ end_date, info=True) }}
+
+
+{% if start_date and end_date %}
+    {{ log("Running with start_date: " ~ start_date, info=True) }}
+    {{ log("Running with end_date: " ~ end_date, info=True) }}
+
+    {% set formatted_start_date = start_date[:4] ~ '-' ~ start_date[4:6] ~ '-' ~ start_date[6:] %}
+    {% set formatted_end_date = end_date[:4] ~ '-' ~ end_date[4:6] ~ '-' ~ end_date[6:] %}
+
+    {{ log("Formatted start_date: " ~ formatted_start_date, info=True) }}
+    {{ log("Formatted end_date: " ~ formatted_end_date, info=True) }}
+
+    {% set date_array = generate_date_array(start_date, end_date) %}
+
+    
+    {% set partitions_to_replace = [] %}
+    {% for date in date_array %}
+        {% set formatted_date = "date('" ~ date ~ "')" %}
+        {% do partitions_to_replace.append(formatted_date) %}
+    {% endfor %}
+
+{% endif %}
+
 {{
     config(
         enabled= var('conversion_events', false) != false,
