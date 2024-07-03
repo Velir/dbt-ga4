b47c0b1fcfd9ef715eacac7b09a0f39c2233a5f8
Adam Ribaudo <adam.ribaudo@velir.com>
2024-07-03 08:12:56 -0400

    macro for custom item parameters

--- macros/stage_custom_parameters.sql
+++ macros/stage_custom_parameters.sql
@@ -4,3 +4,8 @@
     {% endfor %}
 {% endmacro %}
 
+{% macro stage_custom_item_parameters(custom_item_parameters) %}
+    {% for cip in custom_item_parameters %}
+        ,{{ ga4.unnest_key('i.item_params',  cp.name ,  cp.value_type, cp.rename_to or "default" ) }}
+    {% endfor %}
+{% endmacro %}
