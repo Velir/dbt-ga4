429e7cb847633464f40f5ee84ebe6681ec0315bf
JumboDVDH0 <davy.vanderhorst@jumbo.com>
2024-07-24 18:02:53 +0200

    Disabled test

--- tests/page_location_with_gclid_is_cpc.sql
+++ tests/page_location_with_gclid_is_cpc.sql
@@ -1,6 +1,6 @@
 -- Google has changed the combination of parameters that are used to identify a CPC source in the past.
 -- In order to detect new changes, this test checks that a page_location with a gclid is classified as cpc.
-
+{# 
 {{config(
     severity = 'warn'
 )}}
@@ -12,4 +12,4 @@ where original_page_location like '%gclid%'
     and event_source != 'google'
     and event_medium != 'cpc'
 having sources > 0
-    or mediums > 0
+    or mediums > 0 #}
