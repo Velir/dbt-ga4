55fcd3870f8e69aa106ba6bf6bfddd8c5fc2d79a
Davy van der Horst <davy.vanderhorst@springbokagency.com>
2024-12-12 16:57:17 +0100

    Select all selected brands

--- models/staging/base/base_ga4__events.sql
+++ models/staging/base/base_ga4__events.sql
@@ -60,10 +60,18 @@ with source as (
     {% if is_incremental() and var('end_date') is none %}
         and parse_date('%Y%m%d', left(replace(_table_suffix, 'intraday_', ''), 8)) in ({{ partitions_to_replace | join(',') }})
     {% endif %}
-        -- Add property ID filter
-    {% set property_ids = var('brand_properties')[var('brands')[0]] %}
+    -- Add property ID filter for multiple brands
+    {% set selected_properties = [] %}
+    {% if 'all' in var('brands') %}
+        {% do selected_properties.extend(var('brand_properties')['all']) %}
+    {% else %}
+        {% for brand in var('brands') %}
+            {% do selected_properties.extend(var('brand_properties')[brand]) %}
+        {% endfor %}
+    {% endif %}
+    {{ log("Processing properties: " ~ selected_properties, info=True) }}
     and (
-        {% for property_id in property_ids %}
+        {% for property_id in selected_properties %}
         _table_suffix like '%{{ property_id }}'
         {%- if not loop.last %} or {% endif -%}
         {% endfor %}
