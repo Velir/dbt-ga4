ba26d17b797d03d2b74644a22e8544ab14298d4f
evonweltin@inquirer.com <eva.vonweltin@gmail.com>
2025-11-05 18:38:35 -0500

    add macro

---
+++ macros/include_client_key.sql
@@ -0,0 +1,11 @@
+
+
+-- Add key that captures a combination of stream_id and user_pseudo_id to uniquely identify a 'client' (aka. a device) within a single stream
+{% macro include_client_key(source_cte) %}
+    include_client_key as (
+        select *,
+        to_base64(md5(concat(user_pseudo_id, stream_id))) as client_key
+        from {{ source_cte }}
+    )
+{% endmacro %}
+    
